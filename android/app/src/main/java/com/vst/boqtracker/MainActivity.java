bc61526a6693e447194a5e6958d04569767ea063
Atharva-3 <atharvakhandal668@gmail.com>
2026-09-20 08:51:50 +0000

    a

---
+++ android/app/src/main/java/com/vst/boqtracker/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vst.boqtracker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type {CapacitorConfig} from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vst.boqtracker',
+  appName: 'VST BOQ & Compliance Tracker',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
